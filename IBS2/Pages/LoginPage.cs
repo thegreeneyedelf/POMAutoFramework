dd7a2c05c38ef674170fc37c747bc92a73249b5d
thegreeneyedelf <thegreeneyedelf@gmail.com>
2018-11-13 23:45:29 +0100

    changes

--- IBS2/Pages/LoginPage.cs
+++ IBS2/Pages/LoginPage.cs
@@ -1,11 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 using PageOrientedFramework.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace IBS2.Pages
 {
